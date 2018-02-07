11c16a81b1fb83a3a06e9dee469944540ff85b7e
Dorner <jolain.poirier@gmail.com>
2018-02-07 11:31:08 -0400

    Done!

--- Question-5/Source.cpp
+++ Question-5/Source.cpp
@@ -10,8 +10,9 @@ int main() {
 	int base; // La base numérique avec laquelle on travaille
 	char maxChar = 0; // Représente la valeure max de la base alpha-numériquement
 	char input[32]; // Stocke l'input pour verification et manipulation
-	unsigned long long unpck_bcd = 0; // Contient la valeure BCD
-	unsigned long long result = 0; // Contient la valeure finale en binaire
+	char table[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	unsigned long long value = 0; // Utilise pour faire les calculs de conversion
+	unsigned long long result = 0; // Contient la valeure finale dans la base voulue
 	int decallage = 0; // Utilis� pour faire la conversion int -> bcd. Tiens compte du nombre de bytes a d�caller par la gauche.
 	bool loop = true; // Input validation
 
@@ -37,35 +38,9 @@ int main() {
 	}
 
 	base = atoi(input);
+	maxChar = table[base - 1];
 
-	// Déterminer la valeur maximale
-	if (base <= 10) {
-		maxChar = base;
-	}
-	else {
-		switch (base) {
-		case 11:
-			maxChar = 'A';
-			break;
-		case 12:
-			maxChar = 'B';
-			break;
-		case 13:
-			maxChar = 'C';
-			break;
-		case 14:
-			maxChar = 'D';
-			break;
-		case 15:
-			maxChar = 'E';
-			break;
-		case 16:
-			maxChar = 'F';
-			break;
-		}
-	}
-
-	cout << "Entrez la valeur a convertir en base " << base << ": ";
+	cout << "Entrez la valeur decimale a convertir en base " << base << ": ";
 
 	// Input validation pour le nombre
 	memset(input, '\0', sizeof(input)); // Clear l'array avant l'entrée de nouvelles données
@@ -84,72 +59,41 @@ int main() {
 			else { loop = false; }
 		}
 	}
+
+	value = atoi(input);
 	// ------------------------------
 
 
 	// -------------- 2 -------------
 	// (convertion)
-
-
-
-
+	while (value > 0) {
+		result += (value % base) << decallage;
+		value = value / base;
+		decallage += 4;
+	}
 	// -----------------------------
+
 
 	// ----------- 3 ---------------
 	// (affichage)
 
-	// Comme l'information a été stocké dans un format BCD et puis converti en "binaire", les données
-	// a ce point ci ont subi les transformations suivantes:
-	// (chaque bracket représente 8-bits sur la mémoire
-
-	//					 BCD           Codé binaire
-	// ie: (int) 57 -> [05][07] -> [00000101][00000111]
-
-	bit = 0;
-	bool found = false;
 	char output[32];
 	memset(output, '\0', sizeof(output));
 	int i = 0;
-
+	int temp;
 	// Parcours le long long à partir du least significant bit, 4 bits a la fois
 	while (result > 0) {
 		temp = result & 0b1111;
-		if (base <= 10 || temp < 10) {
-			output[i] = (char)temp;
-		}
-		else {
-			char c = '0';
-			switch (temp) {
-			case 10:
-				c = 'A';
-				break;
-			case 11:
-				c = 'B';
-				break;
-			case 12:
-				c = 'C';
-				break;
-			case 13:
-				c = 'D';
-				break;
-			case 14:
-				c = 'E';
-				break;
-			case 15:
-				c = 'F';
-				break;
-			}
-			output[i] = c;
-		}
+		output[i] = table[temp];
 		i++;
-		result = result >> 8; // Saute les [0000] du BCD
+		result = result >> 4;
 	}
 	i--; // Correction du pointeur pour retourner sur le bon charactère
 
 	cout << "La valeur entree est: ";
 	while (i >= 0) {
 		if (!isalpha(output[i])) {
-			cout << (char)('0' + output[i]);  // Force la conversion en char de output[i]
+			cout << output[i]; 
 		}
 		else {
 			cout << output[i];
